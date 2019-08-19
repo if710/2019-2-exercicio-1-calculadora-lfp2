4f2cb45633246f87cf1820c46b9fea4391268a6d
Lavínia Paganini <laviniapaganini@gmail.com>
2019-08-19 20:27:46 -0300

    Adding the buttons functionality and catching errors.
    Make a toast if catch an exception and adding listeners to each button.

--- calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -2,14 +2,98 @@ package br.ufpe.cin.android.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        // Set the listener of all the digit numbers buttons
+        // to edit the EditText text_cal.
+        btn_0.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '0')
+        }
+
+        btn_1.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '1')
+        }
+        btn_2.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '2')
+        }
+        btn_3.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '3')
+        }
+        btn_4.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '4')
+        }
+        btn_5.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '5')
+        }
+        btn_6.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '6')
+        }
+        btn_7.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '7')
+        }
+        btn_8.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '8')
+        }
+        btn_9.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '9')
+        }
+        btn_Add.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '+')
+        }
+        btn_Subtract.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '-')
+        }
+        btn_Divide.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '/')
+        }
+        btn_Clear.setOnClickListener {
+            text_calc.setText("")
+        }
+        btn_Dot.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '.')
+        }
+        btn_LParen.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + '(')
+        }
+        btn_RParen.setOnClickListener {
+            val editTextHello = text_calc.text.toString()
+            text_calc.setText(editTextHello + ')')
+        }
+        btn_Equal.setOnClickListener {
+            // Try to fill the text_info with the formula and
+            // text_cal with the result. If catch a RuntimeException,
+            // raises a toast.
+            try {
+                val newText = eval(text_calc.text.toString()).toString()
+                text_info.setText(text_calc.text.toString())
+                text_calc.setText(newText)
+            } catch (e: RuntimeException) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     //Como usar a função:
     // eval("2+2") == 4.0
@@ -96,7 +180,7 @@ class MainActivity : AppCompatActivity() {
                     else
                         throw RuntimeException("Função desconhecida: " + func)
                 } else {
-                    throw RuntimeException("Caractere inesperado: " + ch.toChar())
+                    throw RuntimeException("Caractere inesperado: " + ch)
                 }
                 if (eat('^')) x = Math.pow(x, parseFactor()) // potência
                 return x
